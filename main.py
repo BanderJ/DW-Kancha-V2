ab2edbcb4855f262a5e641ffaabfec7254da7685
ivaned_ac <edgarelcodigos@gmail.com>
2024-10-18 17:43:15 -0500

    edgar_mergeado

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import controlador_usuario
 import controlador_productos 
 import controlador_nivelusuario
 import controlador_categoria
+import controlador_marca
+import controlador_modelo
+import controlador_productos
 from bd import conectarse
 from flask import session
 #Para generar claves en hash aleatorias
@@ -191,6 +194,177 @@ def actualizar_categoria():
     nombre = request.form["nombre"]
     controlador_categoria.actualizar_categoria(nombre, id)
     return redirect("/Categoria")
+
+# Marcas
+@app.route('/Marca')
+def marca():
+    lista = controlador_marca.obtener_marcas()
+    return render_template('MantMarca.html', lista=lista)
+
+@app.route('/AgregarMarca')
+def formulario_agregar_marca():
+    return render_template('agregar_marca.html')
+
+@app.route("/guardar_marca", methods=["POST"])
+def guardar_marca():
+    nombre = request.form["nombre"]
+    controlador_marca.insertar_marca(nombre)
+    # De cualquier modo, y si todo fue bien, redireccionar
+    return redirect("/Marca")
+
+@app.route("/eliminar_marca", methods=["POST"])
+def eliminar_marca():
+    controlador_marca.eliminar_marca(request.form["id"])
+    return redirect("/Marca")
+
+@app.route("/formulario_editar_marca/<int:id>")
+def formulario_editar_marca(id):
+    # Obtener la marca por ID
+    marca = controlador_marca.obtener_marca_por_id(id)
+    return render_template("editar_marca.html", marca=marca)
+
+@app.route("/actualizar_marca", methods=["POST"])
+def actualizar_marca():
+    id = request.form["id"]
+    nombre = request.form["nombre"]
+    controlador_marca.actualizar_marca(nombre, id)
+    return redirect("/Marca")
+
+# Modelos
+@app.route('/Modelo')
+def modelo():
+    lista = controlador_modelo.obtener_modelos()
+    return render_template('MantModelo.html', lista=lista)
+
+@app.route('/AgregarModelo')
+def formulario_agregar_modelo():
+    marcas = controlador_marca.obtener_marcas()  # Obtener la lista de marcas para el formulario
+    return render_template('agregar_modelo.html', marcas=marcas)
+
+@app.route("/guardar_modelo", methods=["POST"])
+def guardar_modelo():
+    nombre = request.form["nombre"]
+    idMarca = request.form["idMarca"]  # Asegúrate de que este campo está en el formulario
+    controlador_modelo.insertar_modelo(nombre, idMarca)
+    return redirect("/Modelo")
+
+@app.route("/eliminar_modelo", methods=["POST"])
+def eliminar_modelo():
+    controlador_modelo.eliminar_modelo(request.form["id"])
+    return redirect("/Modelo")
+
+@app.route("/formulario_editar_modelo/<int:id>")
+def formulario_editar_modelo(id):
+    modelo = controlador_modelo.obtener_modelo_por_id(id)
+    marcas = controlador_marca.obtener_marcas()  # Obtener la lista de marcas para el formulario
+    return render_template("editar_modelo.html", modelo=modelo, marcas=marcas)
+
+@app.route("/actualizar_modelo", methods=["POST"])
+def actualizar_modelo():
+    id = request.form["id"]
+    nombre = request.form["nombre"]
+    idMarca = request.form["idMarca"]  # Asegúrate de que este campo está en el formulario
+    controlador_modelo.actualizar_modelo(nombre, idMarca, id)
+    return redirect("/Modelo")
+
+
+@app.route('/Contactanos')
+def contactanos():
+    return render_template("Contactanos.html")
+
+@app.route('/Usuario')
+def usuario():
+    lista = controlador_usuario.obtener_usuario()
+    return render_template('MantUsuario.html',lista=lista)
+
+@app.route('/AgregarUsuario')
+def formulario_agregar_usuario():
+    niveles_usuario = controlador_nivelusuario.obtener_nivelusuario()
+    return render_template('agregar_usuario.html', niveles_usuario = niveles_usuario)
+
+@app.route("/guardar_usuario", methods=["POST"])
+def guardar_usuario():
+    nombre = request.form["nombre"]
+    nroDoc = request.form["numerodocumento"]
+    apePat = request.form["apePat"]
+    apeMat = request.form["apeMat"]
+    correo = request.form["correo"]
+    password = request.form["password"]
+    tipoUsu = 1
+    nivelUsu = request.form["nivel_usuario"]
+    controlador_usuario.insertar_usuario(tipoUsu, nombre, nroDoc, apePat, apeMat, correo, password, controlador_nivelusuario.obtener_nivelusuario_por_nombre(nivelUsu)[0])
+    # De cualquier modo, y si todo fue bien, redireccionar
+    return redirect("/Usuario")
+
+@app.route("/eliminar_usuario", methods=["POST"])
+def eliminar_usuario():
+    controlador_usuario.eliminar_usuario(request.form["id"])
+    return redirect("/Usuario")
+
+@app.route("/formulario_editar_Usuario/<int:id>")
+def formulario_editar_usuario(id):
+    usuario = controlador_usuario.obtener_usuario_por_id(id)
+    niveles_usuario = controlador_nivelusuario.obtener_nivelusuario()
+    return render_template("editar_Usuario.html", usuario=usuario, niveles_usuario = niveles_usuario)
+
+@app.route("/actualizar_usuario", methods=["POST"])
+def actualizar_usuario():
+    id = request.form["id"]
+    nombre = request.form["nombre"]
+    nroDoc = request.form["numerodocumento"]
+    apePat = request.form["apePat"]
+    apeMat = request.form["apeMat"]
+    correo = request.form["correo"]
+    password = request.form["password"]
+    tipoUsu = 1
+    nivelUsu = request.form["nivel_usuario"]
+    controlador_usuario.actualizar_usuario(tipoUsu, nombre, nroDoc, apePat, apeMat, correo, password, controlador_nivelusuario.obtener_nivelusuario_por_nombre(nivelUsu)[0], id)
+    return redirect("/Usuario")
+
+@app.route("/formulario_productos")
+def formulario_producto():
+    productos = controlador_productos.obtener_productos()
+    return render_template("Mantproducto.html", productos=productos)
+
+@app.route("/agregar_producto")
+def formulario_agregar_producto():
+    modelos = controlador_productos.obtener_modelos()  # Obtenemos los modelos
+    tallas = controlador_productos.obtener_tallas()    # Obtenemos las tallas
+    return render_template("agregar_producto.html", modelos=modelos, tallas=tallas)
+
+@app.route("/guardar_producto", methods=["POST"])
+def guardar_producto():
+    nombre = request.form["nombre"]
+    precio = request.form["precio"]
+    stock = request.form["stock"]
+    idModelo = request.form["idModelo"]
+    idTalla = request.form["idTalla"]
+    controlador_productos.insertar_producto(nombre, precio, stock, idModelo, idTalla)
+    return redirect("/formulario_productos")
+
+@app.route("/eliminar_producto", methods=["POST"])
+def eliminar_producto():
+    controlador_productos.eliminar_producto(request.form["id"])
+    return redirect("/formulario_productos")
+
+@app.route("/editar_producto/<int:id>")
+def editar_producto(id):
+    producto = controlador_productos.obtener_producto_por_id(id)
+    modelos = controlador_productos.obtener_modelos()  # Obtenemos los modelos
+    tallas = controlador_productos.obtener_tallas()    # Obtenemos las tallas
+    return render_template("editar_producto.html", producto=producto, modelos=modelos, tallas=tallas)
+
+@app.route("/actualizar_producto", methods=["POST"])
+def actualizar_producto():
+    id = request.form["id"]
+    nombre = request.form["nombre"]
+    precio = request.form["precio"]
+    stock = request.form["stock"]
+    idModelo = request.form["idModelo"]
+    idTalla = request.form["idTalla"]
+    controlador_productos.actualizar_producto(nombre, precio, stock, idModelo, idTalla, id)
+    return redirect("/formulario_productos")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
