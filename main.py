7e146dfdbd048a4a658cdd18e9b31526bf7d4528
Christian Cubas Jaramillo <92747342+ChristianCubas@users.noreply.github.com>
2024-10-22 13:29:46 -0500

    Mejora perfil funcional Christian Cubas

--- main.py
+++ main.py
@@ -215,10 +215,14 @@ def registrarUsuario():
 
 @app.route("/perfil")
 def mostrarPerfil():
-    datos = session.get("usuario", {"nombre":"Invitado","apellidos":"Invitado","correo":"example@email.com","numdoc":"11111111","fechaNac":"20/10/2000","sexo":"M","telefono":"999777666"})
-    fecha_nac = datetime.strptime(datos["fechaNac"], "%a, %d %b %Y %H:%M:%S %Z")
-    formato_fecha = fecha_nac.strftime("%d/%m/%Y")
-    print(datos)
+    datos = session.get("usuario", {"nombre":"Invitado","apellidos":"Invitado","correo":"example@email.com","numdoc":"11111111","fechaNac":"10/10/2000","sexo":"M","telefono":"999777666"})
+    if datos["nombre"] != "Invitado":
+        # Solo formatear la fecha si hay sesión del usuario
+        fecha_nac = datetime.strptime(datos["fechaNac"], "%d/%m/%Y")  # Cambiado a formato adecuado
+        formato_fecha = fecha_nac.strftime("%d/%m/%Y")
+    else:
+        # Devolver la fecha en su formato original si no hay sesión
+        formato_fecha = datos["fechaNac"]
     return render_template("MiPerfil.html", userData=datos,nacimiento=formato_fecha)
     
 @app.route('/login', methods=["POST"])  
