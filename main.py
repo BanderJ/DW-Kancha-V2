19ce77cdc2db998a6576b64e3c467d004e03ba45
Christian Cubas Jaramillo <92747342+ChristianCubas@users.noreply.github.com>
2024-10-23 17:59:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -244,6 +244,9 @@ def validarInicioSesion():
         cursor.execute("SELECT idUsuario, nombre, apePat, apeMat, correo, numdoc,fechaNacimiento,sexo,telefono FROM usuario WHERE correo=%s AND password=%s", (email, contraseña,))
         registro = cursor.fetchone()
 
+        fecha_str = registro[6]
+        fecha_dt = datetime.strptime(fecha_str, "%a, %d %b %Y %H:%M:%S %Z")
+        fecha_formateada = fecha_dt.strftime("%d/%m/%Y")
 
         if registro:  # Verificar si se encontró un registro
             datosUsuario["idUsuario"] = registro[0]
@@ -251,7 +254,7 @@ def validarInicioSesion():
             datosUsuario["apellidos"] = f"{registro[2]} {registro[3]}"
             datosUsuario["correo"] = registro[4]
             datosUsuario["numdoc"] = registro[5]
-            datosUsuario["fechaNac"]=registro[6]
+            datosUsuario["fechaNac"]=fecha_formateada
             datosUsuario["sexo"]=registro[7]
             datosUsuario["telefono"]=registro[8]
             datosUsuario["mensaje"] = "Usuario logueado exitosamente"
